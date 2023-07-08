8bde7683b806e2385ba327e36d5b6e2c8aba4c6d
ShuoWen5656 <2260755767@qq.com>
2023-07-08 11:38:41 +0800

    [httpterminator项目][新建项目]：新建核心类模块开始编码

---
+++ httpterminator-core/src/main/java/top/swzhao/project/httpterminatorcore/Main.java
@@ -0,0 +1,16 @@
+package top.swzhao.project.httpterminatorcore;
+
+/**
+ * @author swzhao
+ * @data 2023/7/8 11:37
+ * @Discreption <>
+ */
+public class Main {
+
+
+    public static void main(String[] args) {
+        System.out.println("hello word!");
+    }
+
+
+}
