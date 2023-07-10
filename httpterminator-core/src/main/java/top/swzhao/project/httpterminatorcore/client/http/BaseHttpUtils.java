f1b0209689dadec3083b8c6c6d41b173cd81c072
ShuoWen5656 <2260755767@qq.com>
2023-07-10 22:11:29 +0800

    [httpterminator项目][新建项目]：1、httputils文件建立2、引入httpjar

---
+++ httpterminator-core/src/main/java/top/swzhao/project/httpterminatorcore/client/http/BaseHttpUtils.java
@@ -0,0 +1,13 @@
+package top.swzhao.project.httpterminatorcore.client.http;
+
+/**
+ * @author swzhao
+ * @data 2023/7/9 11:48
+ * @Discreption <> http-core 调用核心
+ */
+public class BaseHttpUtils {
+
+
+
+
+}
---
+++ httpterminator-core/src/main/java/top/swzhao/project/httpterminatorcore/constants/CommonConstants.java
@@ -0,0 +1,33 @@
+package top.swzhao.project.httpterminatorcore.constants;
+
+import org.apache.http.entity.ContentType;
+
+import java.util.HashMap;
+
+/**
+ * @author swzhao
+ * @data 2023/7/9 11:49
+ * @Discreption <> 公共常量
+ */
+public class CommonConstants {
+
+    /**
+     * key - Content-Type
+     */
+    public static final String KEY_CONTENT_TYPE = "Content-Type";
+
+    //public static final String
+
+
+
+
+    public static void main(String[] args) {
+        HashMap<Object, Object> objectObjectHashMap = new HashMap<>();
+        ContentType.APPLICATION_XML.getMimeType();
+        Object o = new Object();
+        objectObjectHashMap.put(o, "456");
+    }
+
+
+
+}
