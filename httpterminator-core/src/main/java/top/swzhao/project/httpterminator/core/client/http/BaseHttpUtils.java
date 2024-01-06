cd3d697cdd587086e00ff886abc18c60b613dc65
ShuoWen5656 <2260755767@qq.com>
2024-01-06 14:36:49 +0800

    [新增代码]1、测试提交

--- httpterminator-core/src/main/java/top/swzhao/project/httpterminator/core/client/http/BaseHttpUtils.java
+++ httpterminator-core/src/main/java/top/swzhao/project/httpterminator/core/client/http/BaseHttpUtils.java
@@ -1,11 +1,15 @@
 package top.swzhao.project.httpterminator.core.client.http;
 
+import lombok.extern.slf4j.Slf4j;
+
 /**
  * @author swzhao
  * @data 2023/7/9 11:48
  * @Discreption <> http-core 调用核心
  */
+@Slf4j
 public class BaseHttpUtils {
+
 
 
 
---
+++ httpterminator-core/src/main/java/top/swzhao/project/httpterminator/core/enuminterface/EnumInterface.java
@@ -0,0 +1,10 @@
+package top.swzhao.project.httpterminator.core.enuminterface;
+
+/**
+ * @author swzhao
+ * @data 2024/1/6 14:35
+ * @Discreption <> 描述创建一个外部资源应该具备的能力
+ */
+public interface EnumInterface {
+
+}
